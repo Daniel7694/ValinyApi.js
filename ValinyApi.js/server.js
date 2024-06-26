eb01b205fda0e4250de02cbae9d790ad93bd221e
Daniel7694 <107689067+Daniel7694@users.noreply.github.com>
2024-06-25 19:28:42 -0500

    Update server.js

--- ValinyApi.js/server.js
+++ ValinyApi.js/server.js
@@ -13,7 +13,7 @@ const cursosRoutes = require('./routes/CursosRoutes');
 
 
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3001; 
 
 app.use(logger('dev'));
 app.use(express.json());
